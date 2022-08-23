a32ae37e13b8c545e608dba864b5154e045dc3fb
Karina Huizar <karinahuizar@gmail.com>
2022-08-23 14:06:52 -0500

    Added more styling for quiz application.

--- src/components/QuestionCard.styles.ts
+++ src/components/QuestionCard.styles.ts
@@ -42,7 +42,12 @@ button {
             : !correct && userClicked
                 ? 'linear-gradient(90deg, #ff5656, #c16868)'
                 : 'linear-gradient(90deg, #56ccff, #6eafb4)'
-    }
+    };
+    border: 3px solid #fff
+    box-shadow: 1px 2px 0px rgbs(0, 0, 0, 0.1);
+    border-radius: 10px;
+    color: #fff;
+    text-shadow: 0px 1px 0px rgba(0, 0, 0, 0.25);
 }
 
 `;
