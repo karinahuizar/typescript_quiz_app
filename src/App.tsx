1681fdcf52e71f5128ad2b8e272f0763f6f4b298
Karina Huizar <karinahuizar@gmail.com>
2022-08-23 08:48:53 -0500

    Added some scaffolding for the quiz application.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+// Components
+import QuestionCard from './components/QuestionCard';
 
 const App = () => {
 
@@ -14,7 +16,18 @@ const App = () => {
 
   }
 
-  return <div className="App">Quiz</div>;
+  return (<div className="App">
+    <h1>REACT QUIZ</h1>
+    <button className='start' onClick={startTrivia}>
+      Start
+    </button>
+    <p className='score'>Score:</p>
+    <p>Loading Questions...</p>
+    <QuestionCard />
+    <button className='next' onClick={nextQuestion}>
+      Next Question
+    </button>
+  </div>);
 }
 
 export default App;
