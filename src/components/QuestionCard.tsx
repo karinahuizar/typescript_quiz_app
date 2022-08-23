ae8d6495e096a02dbe268a2f9f1a54c831b09204
Karina Huizar <karinahuizar@gmail.com>
2022-08-23 08:57:03 -0500

    Created the 'Props' type.

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,5 +1,16 @@
 import React from "react";
 
-const QuestionCard = () => <div>Question Card</div>
+type Props = {
+    question: string;
+    answer: string[];
+    callback: any;
+    userAnswer: string;
+    questionNr: number;
+    totalQuestions: number;
+}
+
+const QuestionCard = () => (
+    <div>Question Card</div>
+);
 
 export default QuestionCard;
