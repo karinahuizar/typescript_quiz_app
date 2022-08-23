87d85e23ff05347d9e28b7e32c80b53c58711a97
Karina Huizar <karinahuizar@gmail.com>
2022-08-23 09:33:55 -0500

    Added the states for the application.

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,38 @@
-import React from 'react';
+import React, { useState } from "react";
 // Components
-import QuestionCard from './components/QuestionCard';
+import QuestionCard from "./components/QuestionCard";
 
 const App = () => {
 
-  const startTrivia = async () => {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
 
-  }
+  const startTrivia = async () => { };
 
-  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => { };
 
-  }
+  const nextQuestion = () => { };
 
-  const nextQuestion = () => {
-
-  }
-
-  return (<div className="App">
-    <h1>REACT QUIZ</h1>
-    <button className='start' onClick={startTrivia}>
-      Start
-    </button>
-    <p className='score'>Score:</p>
-    <p>Loading Questions...</p>
-    <QuestionCard />
-    <button className='next' onClick={nextQuestion}>
-      Next Question
-    </button>
-  </div>);
-}
+  return (
+    <div className="App">
+      <h1>REACT QUIZ</h1>
+      <button className="start" onClick={startTrivia}>
+        Start
+      </button>
+      <p className="score">Score:</p>
+      <p>Loading Questions...</p>
+      <QuestionCard
+        questionNr={number + 1}
+      />
+      <button className="next" onClick={nextQuestion}>
+        Next Question
+      </button>
+    </div>
+  );
+};
 
 export default App;
